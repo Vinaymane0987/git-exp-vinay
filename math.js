0e9e59ce5de6788908b5cca98248c711e368cb95
Vinayak Mane <vinayakmane0987@gmail.com>
2023-08-14 12:53:16 +0530

    added info about ai

--- math.js
+++ math.js
@@ -1,1 +1,3 @@
 // this file will contain add , sub , mul and div functions.
+const ai ="mark my word ai is far more dangerous than others. and the future is changing so drastically that one day ai will take over the world for sure.";
+
