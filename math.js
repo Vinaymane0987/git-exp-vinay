ce6e28df07eee5813ae03998661aac99e738c0e2
Vinayak Mane <vinayakmane0987@gmail.com>
2023-08-14 12:53:42 +0530

    added console.log method

--- math.js
+++ math.js
@@ -1,3 +1,4 @@
 // this file will contain add , sub , mul and div functions.
 const ai ="mark my word ai is far more dangerous than others. and the future is changing so drastically that one day ai will take over the world for sure.";
 
+console.log(ai);
