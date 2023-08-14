d3975fb2adcfdad4062c77a3ff0ba2d27f47d7dc
Vinayak Mane <vinayakmane0987@gmail.com>
2023-08-14 11:02:22 +0530

    added jsdoc

--- math.js
+++ math.js
@@ -1,5 +1,10 @@
 // this file will contain add , sub , mul and div functions.
-
+/**
+ * adds to numbers and returns the results.
+ * @param {number} a 
+ * @param {number} b 
+ * @returns the result
+ */
 const add = (a , b) => {
     return a + b;
 }
