94704dca2882df15f82e2d03886d7b6843fc11ac
Vinayak Mane <vinayakmane0987@gmail.com>
2023-08-14 11:18:09 +0530

    substract function is added

--- math.js
+++ math.js
@@ -1,1 +1,11 @@
 // this file will contain add , sub , mul and div functions.
+
+/**
+ * substract two numbers and returns the result
+ * @param {number} a 
+ * @param {number} b 
+ * @returns the result
+ */
+const sub = (a , b) => {
+    return a - b;
+}
