37ac5c8160ab5062e03455f2bc0ce6061d990815
Vinayak Mane <vinayakmane0987@gmail.com>
2023-08-14 12:02:56 +0530

    added multiply function wich returns the result

--- math.js
+++ math.js
@@ -1,1 +1,16 @@
 // this file will contain add , sub , mul and div functions.
+
+/**
+ * multiply two numbers and returns the result
+ * @param {number} a
+ * @param {number} b
+ * @returns the result
+ */
+const mul = (a, b) => {
+  return a * b;
+};
+
+const testMul = () => {
+  const expected = 10;
+  asert(mul(2, 5) === expected, "should return the expected result.");
+};
