2484f0ea39711908817c9941ea305a2ed746acab
Vinayak Mane <vinayakmane0987@gmail.com>
2023-08-14 12:09:02 +0530

    added jsdocs to div function

--- math.js
+++ math.js
@@ -1,5 +1,11 @@
 // this file will contain add , sub , mul and div functions.
 
+/**
+ * it divides the two numbers and return the result.
+ * @param {number} a 
+ * @param {number} b 
+ * @returns the result
+ */
 const div = (a , b) => {
     return a / b;
 }
